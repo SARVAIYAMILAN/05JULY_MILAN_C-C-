bdb2b6e2fcce5ddf4d2ae19a54df22c242cb5f5d
SARVAIYAMILAN <109734789+SARVAIYAMILAN@users.noreply.github.com>
2022-08-03 16:25:40 +0530

    Add files via upload

---
+++ ASSIGNMENT/MODULE3/PYRAMIDP.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k;
+	clrscr();
+	for(i=1;i<=4;i++)
+	{
+		for(j=4;j<=1;j--)
+		{
+			printf("* ");
+		}
+		for(k=1;k<=i;k++)
+		{
+			printf("* ");
+		}
+		{	printf("\n");
+		}
+		getch();
+
+
+
+	}
+
+}
