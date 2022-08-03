fe2496fdc463c9598d9aeff7c8b47248e0df47b6
SARVAIYAMILAN <109734789+SARVAIYAMILAN@users.noreply.github.com>
2022-08-03 15:59:57 +0530

    Add files via upload

---
+++ ASSIGNMENT/MODULE2/CUBE&SQU (1).C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	printf("enter a number");
+	scanf("%d",&n);
+	printf("\nsqur of number %d is %d",n,n*n);
+	printf("\ncube of number %d is %d",n,n*n*n);
+	getch();
+
+
+}
