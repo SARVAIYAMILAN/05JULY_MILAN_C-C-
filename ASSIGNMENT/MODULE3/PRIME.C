fd1686e2ebff85cb3c04df3117565c8aaf601010
SARVAIYAMILAN <109734789+SARVAIYAMILAN@users.noreply.github.com>
2022-08-05 16:57:27 +0530

    Add files via upload

---
+++ ASSIGNMENT/MODULE3/PRIME.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	
+	int n, count=0 ,i;
+	printf("enter the value:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+	
+		
+			 if(count==2)
+		    {
+			printf("\nprime number");
+		    }
+        }
+		else
+
+		{
+			
+			printf("\nnot prime number");
+		}
+		
+	}
+	return 0;
+}
