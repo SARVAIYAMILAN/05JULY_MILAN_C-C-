c2733d3579f054e55cf21c08e0387bb99c4b9ed1
SARVAIYAMILAN <109734789+SARVAIYAMILAN@users.noreply.github.com>
2022-08-26 15:59:04 +0530

    Add files via upload

---
+++ TASK/WITHPARA.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//#nclude<conio.h>
+void add(int a,int b);
+
+int main()
+{
+add(10,20);
+//getch();
+return 0;
+
+
+}
+void add(int a,int b)
+{
+
+
+	int result=a+b;
+	printf("the result of %d and %d is %d",a,b,result);
+	
+}
