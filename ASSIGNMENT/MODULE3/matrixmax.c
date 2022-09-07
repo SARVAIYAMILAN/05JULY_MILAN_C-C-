586846bfb6381c99c927d02a5a0239c50abe90a0
SARVAIYAMILAN <109734789+SARVAIYAMILAN@users.noreply.github.com>
2022-09-07 16:05:05 +0530

    Add files via upload

---
+++ ASSIGNMENT/MODULE3/matrixmax.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,max,a[3][3];
+	printf("enter the matrix :");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		
+	}
+	printf("the first matrix\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	max=a[0][0];
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			if(max<a[i][j])
+			{
+				max=a[i][j];
+			}
+		}
+	}
+	printf("max matrix is %d\n",max);
+	return 0;
+}
